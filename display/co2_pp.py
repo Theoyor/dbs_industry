6288213d95f52b0e403c91e19d1330fed03456cb
juliusclausnitzer <julius.clausnitzer@gmail.com>
2021-07-04 11:52:11 +0200

    co2_pp

---
+++ display/co2_pp.py
@@ -0,0 +1,15 @@
+import json
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+import select_statement as ss
+
+
+df = ss.select(["year_id", "country_id", "emission", "population", "emission_pp"], ["country_in_year"] )
+fig = px.line(df, x= "year_id", y="emission_pp", color="country_id")
+fig.update_layout(bargap=0.2)
+fig.show()
